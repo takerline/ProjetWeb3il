b0bb5e54c4340628b7aa08e8cb16d98038584922
takerline <takerline@orange.fr>
2024-01-24 08:48:03 +0100

    add docker

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
   
   <body>
   
-    <h1>!! à Faire !!</h1>
+    <h1>!! à Fairdoe !!</h1>
 	
 	
 	
 	
 	
 	
-	
+  
 	
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
